e6396c6aae9b2cf37300bdffb6346420af78f1ea
jeswin <jeswinpeter92@gmail.com>
2025-02-16 20:49:47 +0530

    Trying out resizing

--- Read_img.py
+++ Read_img.py
@@ -1,7 +1,36 @@
 import cv2 as cv
 
-img = cv.imread('D:/HTML CSS/IMAGES/minions_image.jpg')
+# ---- Frame scaling Function ---- #
+def resize(frame, scale = 0.5):
+    width = int(frame.shape[1] * scale )
+    height = int(frame.shape[0] * scale )
+
+    dimensions = (width,height)
+
+    return cv.resize(frame, dimensions, interpolation = cv.INTER_AREA)
+    
+
+# ---- Image Reading ---- #
+
+img = resize(cv.imread('D:/HTML CSS/IMAGES/minions_image.jpg'))
 
 cv.imshow('minions', img)
-
 cv.waitKey(0)
+
+
+# --- Viedeo Reading --- #
+
+capture = cv.VideoCapture('A:/Download/Manichithra thazhu thilakan dialogues.mp4')
+
+while True:
+    isTrue, frame = capture.read()
+
+    new_frame = resize(frame)
+    cv.imshow('Video', frame)
+    cv.imshow('New Frame', new_frame)
+
+    if cv.waitKey(20) & 0xFF == ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
