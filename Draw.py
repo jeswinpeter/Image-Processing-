d1b58d0c97eea043f2773513d2e3860d5f1cdfa2
jeswin <jeswinpeter92@gmail.com>
2025-02-22 22:20:20 +0530

    Added Line method

--- Draw.py
+++ Draw.py
@@ -18,9 +18,13 @@ blank = np.zeros((600,1000,3), dtype='uint8')
 # cv.imshow('Blank', blank)
 
 blank[:] = 15,100,230
-# cv.imshow('Red',blank)
 
-cv.rectangle(blank, (10,10), (250,300), (50,150,200), thickness=4)
-cv.imshow('Red',blank)
+cv.rectangle(blank, (100,100), (300,300), (50,150,200), thickness=-1)
+#cv.imshow('Red',blank)
+
+cv.circle(blank, (blank.shape[1]//2, blank.shape[0]//2), 40,(150,100,250), thickness=-1)
+
+cv.line(blank, (0,0), (blank.shape[1]//2, blank.shape[0]//2),(150,10,250), thickness=2)
+cv.imshow('Line', blank)
 
 cv.waitKey(0)
