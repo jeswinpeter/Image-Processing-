3864a21c6d570cee763ca35c4eeee8d33416ebd6
jeswin <jeswinpeter92@gmail.com>
2025-02-23 14:37:35 +0530

    Writing on an image

--- Draw.py
+++ Draw.py
@@ -9,22 +9,44 @@ def resize(frame, scale = 0.55):
     dimensions = (width,height)
 
     return cv.resize(frame, dimensions, interpolation = cv.INTER_AREA)
+# -----------------------------------#
 
 
 # image = resize(cv.imread('D:/HTML CSS/IMAGES/TEST_banner.png'))
 # cv.imshow('IMAGE', image)
 
+
+# ------ Creating a blank window where you can work---
 blank = np.zeros((600,1000,3), dtype='uint8')
 # cv.imshow('Blank', blank)
+# ---------------------------------------------------
 
 blank[:] = 15,100,230
 
+
+# --------- Drawing a rectangle -----------
 cv.rectangle(blank, (100,100), (300,300), (50,150,200), thickness=-1)
 #cv.imshow('Red',blank)
+# -----------------------------------------
 
+
+# ------ Drawing a Circle of radius 40 -------------
 cv.circle(blank, (blank.shape[1]//2, blank.shape[0]//2), 40,(150,100,250), thickness=-1)
+# --------------------------------------------------
 
-cv.line(blank, (0,0), (blank.shape[1]//2, blank.shape[0]//2),(150,10,250), thickness=2)
-cv.imshow('Line', blank)
 
+# -------------- Drawing a line ------------
+cv.line(blank, (0,500), (blank.shape[1]//2, blank.shape[0]//2),(150,10,250), thickness=2)
+
+# ------------------------------------------
+
+
+# ---------- Writing text on image -----------
+cv.putText(blank, 'Bazinga', (500,200), cv.FONT_HERSHEY_DUPLEX, 1.2, (20,5,5), 2)
+cv.imshow('Text', blank)
+# --------------------------------------------
+
+
+# -- Closes when any key is pressed -- 
 cv.waitKey(0)
+# ------------------------------------
